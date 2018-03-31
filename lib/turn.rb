b275813935db54def956fab4873c76d2a40d6d2b
Damitha Ranasingha <rmddcr@gmail.com>
2018-03-31 13:02:35 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,12 @@
 def turn(board)
   puts "Please enter 1-9:"
+  input=gets
+  index=input_to_index(input)
+  if valid_movie?(board,index)
+    move(board,index)
+  else
+    turn(board)
+  end
 end
 
 
